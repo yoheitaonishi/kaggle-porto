6e594a641091339dcd08f481777bb3268f352c55
yoheitaonishi <onishiyoheita@gmail.com>
2018-06-22 05:24:09 +0000

    cv

--- protos/train.py
+++ protos/train.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 from logging import StreamHandler, DEBUG, Formatter, FileHandler, getLogger
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import log_loss, roc_auc_score
+
 from load_data import load_train_data, load_test_data
 
 logger = getLogger(__name__)
@@ -35,6 +38,31 @@ if __name__ == '__main__':
     logger.debug('train columns: {} {}'.format(use_cols.shape, use_cols))
 
     logger.info('data preparation end {}'.format(x_train.shape))
+
+    cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=0)
+
+    list_auc_score = []
+    list_logloss_score = []
+
+    for train_idx, valid_idx in cv.split(x_train, y_train):
+        trn_x = x_train.iloc[train_idx, :]
+        val_x = x_train.iloc[valid_idx, :]
+
+        trn_y = y_train[train_idx]
+        val_y = y_train[valid_idx]
+
+        clf = LogisticRegression(random_state=0)
+        clf.fit(trn_x, trn_y)
+
+        pred = clf.predict_proba(val_x)[:, 1]
+        sc_logloss = log_loss(val_y, pred)
+        sc_auc = roc_auc_score(val_y, pred)
+
+        list_logloss_score.append(sc_logloss)
+        list_auc_score.append(sc_auc)
+        logger.debug('    logloss: {}, auc:{}'.format(sc_logloss, sc_auc))
+
+    logger.debug('logloss: {}, auc:{}'.format(np.mean(list_logloss_score), np.mean(list_auc_score)))
 
     clf = LogisticRegression(random_state=0)
     clf.fit(x_train, y_train)
